e60ce53547d2f7201332c84826bc08d901f22ab8
l-freeze <actions@example.com>
2024-07-13 21:55:52 +0000

    Initializing publish branch

---
+++ assets/404-DAl6Pgc5.js
@@ -0,0 +1,1 @@
+import{t}from"./index-eLw3SFb9.js";var o=t('<section class="text-gray-700 p-8"><h1 class="text-2xl font-bold">404: Not Found</h1><p class=mt-4>🔙🐈');function s(){return o()}export{s as default};
---
+++ assets/profile-CC7AqJwR.js
@@ -0,0 +1,17 @@
+import{s as a,a as l,c as i,t as c,b as f,m,g as p,d as u,e as h,f as g}from"./index-eLw3SFb9.js";var $=c('<div class="prose prose-stone">');const d="l-freeze",b="github.com/l-freeze",v="プログラマー",P="性善説",z="互いにありがとうと言える関係で、互いに提供し合う関係で",H="思想の合う人、または異なる思想を持っていても互いに敬意を払える部分を見出し尊重し合える人",k=`
+**l-freezeについて** 
+
+- name
+    - ${d}
+- github
+    - ${b}
+- job
+    - ${v}
+       - デザイン以外なら何でも
+- philosophy
+    - ${P}
+    - ${z}
+- okAt
+    - ${H}
+
+`,x=()=>{a("Profile"),l({title:"l-freeze"});const t=()=>{const[e,r]=f("");return[e,n=>r(m(n,{extensions:[p(),u()],htmlExtensions:[h(),g({})],allowDangerousHtml:!0}))]},[o,s]=t();return s(k),(()=>{var e=$();return i(()=>e.innerHTML=o()),e})()};function j(){return x()}export{x as Profile,j as default};
